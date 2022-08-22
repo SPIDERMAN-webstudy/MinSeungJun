0f053f97eb1d40a61b60abdda528de7a6ac6820b
Seugnjun <minsj47@gmail.com>
2022-08-22 23:24:10 +0900

    John Ahn boiler-plate with node

---
+++ boiler-plate/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://StormMin:x789NrwcgbmureG@boilerplate.bbk3vyl.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
